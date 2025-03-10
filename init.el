f0986bc9fab13ce34f017325f883cc7c96d805e1
Taylan Dogan <git@taylandogan.info>
2025-03-10 12:49:03 +0100

    update
    Signed-off-by: Taylan Dogan <git@taylandogan.info>

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 
 ;;; Start
 
+;;; Code:
 (setq gc-cons-threshold 402653184
       gc-cons-percentage 0.6)
 
@@ -61,49 +62,59 @@
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
-   ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
+   ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0"
+    "#b2b2b2"])
  '(company-quickhelp-color-background "#4F4F4F")
  '(company-quickhelp-color-foreground "#DCDCCC")
  '(custom-safe-themes
-   '("aded61687237d1dff6325edb492bde536f40b048eab7246c61d5c6643c696b7f" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" "a622aaf6377fe1cd14e4298497b7b2cae2efc9e0ce362dade3a58c16c89e089c" "2a9039b093df61e4517302f40ebaf2d3e95215cb2f9684c8c1a446659ee226b9" "e2fd81495089dc09d14a88f29dfdff7645f213e2c03650ac2dd275de52a513de" "08ef1356470a9d3bf363ffab0705d90f8a492796e9db489936de4bde6a4fdb19" "83b1fda71a1cf78a596891c0cc10601e93d5450148f98e9b66dde80349b20195" "217ec62e2aba3b5bf1781b30ac94870c7d8192b24e856c346b98fce4a896046b" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
+   '("aded61687237d1dff6325edb492bde536f40b048eab7246c61d5c6643c696b7f"
+     "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a"
+     "a622aaf6377fe1cd14e4298497b7b2cae2efc9e0ce362dade3a58c16c89e089c"
+     "2a9039b093df61e4517302f40ebaf2d3e95215cb2f9684c8c1a446659ee226b9"
+     "e2fd81495089dc09d14a88f29dfdff7645f213e2c03650ac2dd275de52a513de"
+     "08ef1356470a9d3bf363ffab0705d90f8a492796e9db489936de4bde6a4fdb19"
+     "83b1fda71a1cf78a596891c0cc10601e93d5450148f98e9b66dde80349b20195"
+     "217ec62e2aba3b5bf1781b30ac94870c7d8192b24e856c346b98fce4a896046b"
+     "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476"
+     default))
  '(ediff-diff-options "-w")
  '(ediff-split-window-function 'split-window-horizontally)
  '(ediff-window-setup-function 'ediff-setup-windows-plain)
- '(elcord-mode t nil (elcord))
  '(fci-rule-color "#383838")
  '(line-number-mode nil)
  '(lsp-keymap-prefix "C-c C-l")
  '(nrepl-message-colors
-   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
+   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3"
+     "#94BFF3" "#DC8CC3"))
  '(org-agenda-files
-   '("/home/kondanta/.syncthing/emacs/org-roam/999-daily/2021-04-13.org"))
+   '("/home/taylan/Documents/notes/org-roam/999-daily/2024-07-14.org"))
  '(org-export-backends '(ascii html icalendar latex md odt))
  '(org-super-agenda-mode t)
  '(org-tags-column -80)
  '(package-selected-packages
-   '(json-mode terraform-mode all-the-icons exec-path-from-shell go-autocomplete markdown-mode dart-mode move-text magit-gitflow gruvbox-theme gruvbox gruvbox-dark-hard-theme gruvbox-dark-hard cmake-mode company-go flycheck-gometalinter irony-eldoc magit expand-region mark-multiple dockerfile-mode docker-compose-mode yaml-mode yasnippet ido-vertical-mode company-dart flycheck-rust rust rust-mode go-mode company-c-headers flycheck-clang-analyzer projectile company-irony pretty-mode swiper diminish spaceline company dashboard rainbow-delimiters rainbow-delimeters switch-window rainbow-mode avy smex org-bullets which-key srcery))
+   '(json-mode terraform-mode all-the-icons exec-path-from-shell
+	       go-autocomplete markdown-mode dart-mode move-text
+	       magit-gitflow gruvbox-theme gruvbox
+	       gruvbox-dark-hard-theme gruvbox-dark-hard cmake-mode
+	       company-go flycheck-gometalinter irony-eldoc magit
+	       expand-region mark-multiple dockerfile-mode
+	       docker-compose-mode yaml-mode yasnippet
+	       ido-vertical-mode company-dart flycheck-rust rust
+	       rust-mode go-mode company-c-headers
+	       flycheck-clang-analyzer projectile company-irony
+	       pretty-mode swiper diminish spaceline company dashboard
+	       rainbow-delimiters rainbow-delimeters switch-window
+	       rainbow-mode avy smex org-bullets which-key srcery))
  '(pdf-view-midnight-colors '("#b2b2b2" . "#292b2e"))
  '(terraform-indent-level 4)
  '(vc-annotate-background "#2B2B2B")
  '(vc-annotate-color-map
-   '((20 . "#BC8383")
-     (40 . "#CC9393")
-     (60 . "#DFAF8F")
-     (80 . "#D0BF8F")
-     (100 . "#E0CF9F")
-     (120 . "#F0DFAF")
-     (140 . "#5F7F5F")
-     (160 . "#7F9F7F")
-     (180 . "#8FB28F")
-     (200 . "#9FC59F")
-     (220 . "#AFD8AF")
-     (240 . "#BFEBBF")
-     (260 . "#93E0E3")
-     (280 . "#6CA0A3")
-     (300 . "#7CB8BB")
-     (320 . "#8CD0D3")
-     (340 . "#94BFF3")
-     (360 . "#DC8CC3")))
+   '((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F")
+     (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F")
+     (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F")
+     (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3")
+     (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3")
+     (340 . "#94BFF3") (360 . "#DC8CC3")))
  '(vc-annotate-very-old-color "#DC8CC3"))
 
 (custom-set-faces
@@ -111,8 +122,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#282828" :foreground "#fdf4c1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :family "Sarasa Term Slab CL"))))
- '(markdown-code-face ((t (:inherit Sarasa Term Slab CL))))
- '(org-level-1 ((t (:foreground "#83a598" :family Sarasa Term Slab CL)))))
+ '(default ((t (:inherit nil :stipple nil :background "#282828" :foreground "#fdf4c1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :family "Iosevka Term Slab Nerd Font"))))
+ '(markdown-code-face ((t (:inherit Iosevka Term Slab Nerd Font))))
+ '(org-level-1 ((t (:foreground "#83a598" :family Iosevka Term Slab Nerd Font)))))
 
 ;;; init.el ends here
