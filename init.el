1df90895d7dc2fd78d88968d9fc3631639e427c6
Taylan Dogan <git@taylandogan.info>
2025-07-22 20:25:50 +0200

    feat!: create config from scratch
    Signed-off-by: Taylan Dogan <git@taylandogan.info>

--- init.el
+++ init.el
@@ -1,123 +1,45 @@
+;;;; init.el --- Minimal startup file -*- lexical-binding: t; -*-
 
-;;; Start
+;; Set garbage collection thresholds for faster startup
+(setq gc-cons-threshold (\* 100 1024 1024)
+gc-cons-percentage 0.6)
 
-;;; Code:
-(setq gc-cons-threshold 402653184
-      gc-cons-percentage 0.6)
-
+; Set default protocol for pulling packages to SSH
 (setq straight-vc-git-default-protocol 'ssh)
 
-;; keep the installed packages in .emacs.d
-(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
-(setq package-enable-at-startup nil)
-;; (package-initialize)
 
+;; Defer garbage collection until after startup
+(add-hook 'emacs-startup-hook
+(lambda ()
+(setq gc-cons-threshold (\* 8 1024 1024)
+gc-cons-percentage 0.1)))
+
+;; --- Bootstrap straight.el ---
 (defvar bootstrap-version)
 (let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+(expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+(bootstrap-version 5))
+(unless (file-exists-p bootstrap-file)
+(with-current-buffer
+(url-retrieve-synchronously
+"https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+'silent 'inhibit-cookies)
+(goto-char (point-max))
+(eval-print-last-sexp)))
+(load bootstrap-file nil 'nomessage))
 
-;; (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-
-;; (setq package-archives '(("melpa" . "http://melpa.org/packages/")
-;;                          ("gnu" . "http://elpa.gnu.org/packages/")))
-;; (require 'package)
-;; (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-;; (add-to-list 'package-archives
-;;              '("melpa" . "https://melpa.org/packages/") t)
-
-;; update the package metadata is the local cache is missing
-;; (unless package-archive-contents
-;;   (package-refresh-contents))
+;; --- Configure use-package ---
 (straight-use-package 'use-package)
-;; (straight-use-package 'org-contrib)
 (setq straight-use-package-by-default t)
-;; (unless (package-installed-p 'use-package)
-;;   (package-refresh-contents)
-;;   (package-install 'use-package))
+(setq use-package-always-ensure t) ;; Ensures packages are installed by default
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;config.org linker. For manipulating config in a nice way.
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+;; --- Load the main configuration ---
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0"
-    "#b2b2b2"])
- '(company-quickhelp-color-background "#4F4F4F")
- '(company-quickhelp-color-foreground "#DCDCCC")
- '(custom-safe-themes
-   '("aded61687237d1dff6325edb492bde536f40b048eab7246c61d5c6643c696b7f"
-     "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a"
-     "a622aaf6377fe1cd14e4298497b7b2cae2efc9e0ce362dade3a58c16c89e089c"
-     "2a9039b093df61e4517302f40ebaf2d3e95215cb2f9684c8c1a446659ee226b9"
-     "e2fd81495089dc09d14a88f29dfdff7645f213e2c03650ac2dd275de52a513de"
-     "08ef1356470a9d3bf363ffab0705d90f8a492796e9db489936de4bde6a4fdb19"
-     "83b1fda71a1cf78a596891c0cc10601e93d5450148f98e9b66dde80349b20195"
-     "217ec62e2aba3b5bf1781b30ac94870c7d8192b24e856c346b98fce4a896046b"
-     "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476"
-     default))
- '(ediff-diff-options "-w")
- '(ediff-split-window-function 'split-window-horizontally)
- '(ediff-window-setup-function 'ediff-setup-windows-plain)
- '(fci-rule-color "#383838")
- '(line-number-mode nil)
- '(lsp-keymap-prefix "C-c C-l")
- '(nrepl-message-colors
-   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3"
-     "#94BFF3" "#DC8CC3"))
- '(org-agenda-files
-   '("/home/taylan/Documents/notes/org-roam/999-daily/2024-07-14.org"))
- '(org-export-backends '(ascii html icalendar latex md odt))
- '(org-super-agenda-mode t)
- '(org-tags-column -80)
- '(package-selected-packages
-   '(json-mode terraform-mode all-the-icons exec-path-from-shell
-	       go-autocomplete markdown-mode move-text
-	       magit-gitflow cmake-mode
-	       company-go flycheck-gometalinter irony-eldoc magit
-	       expand-region mark-multiple dockerfile-mode
-	       docker-compose-mode yaml-mode yasnippet
-	       ido-vertical-mode company-dart flycheck-rust rust
-	       rust-mode go-mode company-c-headers
-	       flycheck-clang-analyzer projectile company-irony
-	       pretty-mode swiper diminish spaceline company dashboard
-	       rainbow-delimiters rainbow-delimeters switch-window
-	       rainbow-mode avy smex org-bullets which-key srcery))
- '(pdf-view-midnight-colors '("#b2b2b2" . "#292b2e"))
- '(terraform-indent-level 4)
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map
-   '((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F")
-     (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F")
-     (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F")
-     (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3")
-     (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3")
-     (340 . "#94BFF3") (360 . "#DC8CC3")))
- '(vc-annotate-very-old-color "#DC8CC3"))
+;; --- Custom File ---
+;; All variables set via M-x customize will be stored here, not in init.el.
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+(load custom-file))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#282828" :foreground "#fdf4c1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :family "Iosevka Term Slab Nerd Font"))))
- '(markdown-code-face ((t (:inherit Iosevka Term Slab Nerd Font))))
- '(org-level-1 ((t (:foreground "#83a598" :family Iosevka Term Slab Nerd Font)))))
-
-;;; init.el ends here
+;;;; init.el ends here
